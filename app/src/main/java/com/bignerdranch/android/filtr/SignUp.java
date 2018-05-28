fdd9c093b3786429272804257daf5ebb55c5cf01
David Scanlon <dscanlon@uncc.edu>
2018-05-28 18:35:23 -0400

    added signin screens

--- app/src/main/java/com/bignerdranch/android/filtr/SignUp.java
+++ app/src/main/java/com/bignerdranch/android/filtr/SignUp.java
@@ -2,12 +2,29 @@ package com.bignerdranch.android.filtr;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class SignUp extends AppCompatActivity {
+
+    private Button signUpButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        signUpButton = findViewById(R.id.button_signUp_signUp);
+        signUpButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                
+                finish();
+            }
+        });
+
+
     }
+
+
 }
